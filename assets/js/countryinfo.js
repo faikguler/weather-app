9bf2f5e1e43e34a4846319516125e018574cf1bf
Eve-LynBremner <emvanschoor@gmail.com>
2026-02-05 20:05:42 +0000

    update country info js and html

--- assets/js/countryinfo.js
+++ assets/js/countryinfo.js
@@ -19,11 +19,11 @@ function onfetchInfo() {
       })
       .then((data) => {
         renderPopulation(data);
+        renderLanguage(data);
         renderRegion(data);
         renderCapital(data);
-        // renderPopulation(data);
-        // renderMap(data);
-        // renderLanguage(data);
+        renderFlag(data);
+        renderMap(data);        
       })
       .catch((error) => {
         console.error("Error:", error.message);
@@ -33,9 +33,29 @@ function onfetchInfo() {
   }
 }
 
+const renderFlag = (country) => {
+  const countryFlagEl = document.getElementById("flag");
+  let html = "";
+  
+  country.forEach((countries) => {
+    const countryFlag = countries.flags.png;
+    html += `<img src=${countryFlag}>`;
+  });
 
-//       const map = country.maps.googleMaps
-//   const languages = country.languages
+  countryFlagEl.innerHTML = html;
+};
+
+const renderMap = (country) => {
+  const countryMapEl = document.getElementById("map");
+  let html = "";
+  
+  country.forEach((countries) => {
+    const countryMap = countries.maps.googleMaps;
+    html += `<li><a href=${countryMap}>Google Map</a></li>`;
+  });
+
+  countryMapEl.innerHTML = html;
+};
 
 const renderPopulation = (country) => {
   const countryPopEl = document.getElementById("population");
@@ -50,15 +70,15 @@ const renderPopulation = (country) => {
 };
 
 const renderCapital = (country) => {
-  const countryPopEl = document.getElementById("capital");
+  const countryCapEl = document.getElementById("capital");
   let html = "";
   
   country.forEach((countries) => {
-    const countryCapital = countries.capital;
+    const countryCapital = countries.capital.join(", "); // adds comma and spaces if more than one capital
     html += `<li>Capital: ${countryCapital}</li>`;
   });
 
-  countryPopEl.innerHTML = html;
+  countryCapEl.innerHTML = html;
 };
 
 const renderRegion = (country) => {
@@ -67,8 +87,21 @@ const renderRegion = (country) => {
   
   country.forEach((countries) => {
     const countryRegion = countries.region;
+    console.log(countryRegion);
     html += `<li>Region: ${countryRegion}</li>`;
   });
 
   countryRegEl.innerHTML = html;
+};
+
+const renderLanguage = (country) => {
+  const countryLangEl = document.getElementById("language");
+  let html = "";
+
+  country.forEach((countries) => {
+    const countryLanguage = Object.values(countries.languages).join(", "); // Postman shows that Languages returns an object. Object.values returns an array of the values of an object
+    html += `<li>Language: ${countryLanguage}</li>`;
+    })
+
+  countryLangEl.innerHTML = html;
 };
